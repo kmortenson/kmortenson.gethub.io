4b39ea0ec8d1c9a3a8f6865a70f60d4a8da443f5
Kirk Mortenson <kmortenson@byui.edu>
2019-04-01 23:46:53 -0700

    push

--- assignments/lesson-9/scripts/towndata.js
+++ assignments/lesson-9/scripts/towndata.js
@@ -8,14 +8,19 @@ request.onload = function () {
     var townData = request.response;
     showData(townData);
 }
+
 function showData(jsonObj) {
     var data = jsonObj['towns'];
 
+    /* for (var i = 0; i < data.length; i++) {
+         if (i === 0 || i === 2 || i === 3 || i === 6) {
+             continue;
+         }
+    */
     for (var i = 0; i < data.length; i++) {
-        if (i === 0 || i === 2 || i === 3 || i === 6) {
+        if ( i == 2 || i == 5 || i == 6) {
             continue;
         }
-        
         var myArticle = document.createElement('div');
         var myH5 = document.createElement('h5');
         var myPara1 = document.createElement('h6');
@@ -23,7 +28,7 @@ function showData(jsonObj) {
         var myPara3 = document.createElement('p');
         var myPara4 = document.createElement('p');
         var myPhoto = document.createElement('img');
-   
+
         myH5.textContent = data[i].name;
         myPara1.textContent = data[i].motto;
         myPara2.textContent = 'Year Founded: ' + data[i].yearfounded;
@@ -36,20 +41,22 @@ function showData(jsonObj) {
         myArticle.appendChild(myPara3);
         myArticle.appendChild(myPara4);
         myArticle.appendChild(myPhoto);
-        if (i === 1)
+        if (i === 2)
             myPhoto.scr = "images/home2.jpg"
         myPhoto.setAttribute('class', 'homePhoto');
         myPhoto.setAttribute('alt', 'Fish Haven Photo');
-        if (i === 4)
+        if (i === 5)
             myPhoto.scr = "images/home1.jpg"
         myPhoto.setAttribute('class', 'homePhoto');
         myPhoto.setAttribute('alt', 'Preston Photo');
-        if (i === 5)
+        if (i === 6)
             myPhoto.scr = "images/home3.jpg"
         myPhoto.setAttribute('class', 'homePhoto');
         myPhoto.setAttribute('alt', 'Soda Springs Photo');
-       
-        section.appendChild(myArticle);
-    }
 
+        section.appendChild(myArticle);
+
+
+    }
+    
 }
