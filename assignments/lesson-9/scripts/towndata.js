cf0489af6c6644672306ade1f604d2441ae8d1f1
Kirk Mortenson <kmortenson@byui.edu>
2019-04-05 00:02:28 -0700

    update

--- assignments/lesson-9/scripts/towndata.js
+++ assignments/lesson-9/scripts/towndata.js
@@ -18,20 +18,20 @@ function showData(jsonObj) {
             continue;
         }
         var myArticle = document.createElement('div');
-        var myH5 = document.createElement('h2');
+        var myH2 = document.createElement('h2');
         var myPara1 = document.createElement('h6');
         var myPara2 = document.createElement('p');
         var myPara3 = document.createElement('p');
         var myPara4 = document.createElement('p');
         var myPhoto = document.createElement('img');
 
-        myH5.textContent = data[i].name;
+        myH2.textContent = data[i].name;
         myPara1.textContent = data[i].motto;
-        myPara2.textContent = 'Year Founded: ' + data[i].yearfounded;
-        myPara2.textContent = 'Current Population: ' + data[i].currentPopulation;
-        myPara2.textContent = 'Average Rain Fall: ' + data[i].averageRainfall;
+        myPara2.textContent = 'Year Founded: ' + data[i].yearFounded;
+        myPara3.textContent = 'Current Population: ' + data[i].currentPopulation;
+        myPara4.textContent = 'Average Rain Fall: ' + data[i].averageRainfall;
 
-        myArticle.appendChild(myH5);
+        myArticle.appendChild(myH2);
         myArticle.appendChild(myPara1);
         myArticle.appendChild(myPara2);
         myArticle.appendChild(myPara3);
