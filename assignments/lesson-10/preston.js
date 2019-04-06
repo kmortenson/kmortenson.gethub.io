9dcfc6ae61af7a3c9bd63f6ac50d235a5b598558
Kirk Mortenson <kmortenson@byui.edu>
2019-04-06 14:20:09 -0700

    javascript

---
+++ assignments/lesson-10/preston.js
@@ -0,0 +1,25 @@
+//Preston 5604473
+
+//weather summery
+var weatherObject = new XMLHttpRequest();
+weatherObject.open('GET', 'HTTPS://api.openweathermap.org/data/2.5/weather?id=5604473&appid-ba0aa9a68bce08fd2b1322f852ff85cc&units -imperial', true);
+weatherObject.send();
+weatherObject.onload = function () {
+    
+    var weatherInfo = json.parse(weatherObject.responseText);
+    console.log(weatherInfo);
+
+    document.getElementById('weatherDesc').innerHTML = weatherInfo.weather[0].description;
+    document.getElementById('cTemp').innerHTML = weatherInfo.main.temp;
+    document.getElementById('humidity').innerHTML = weatherInfo.main.humidity;
+    document.getElementById('windspeed').innerHTML = weatherInfo.wind.speed;
+
+   
+    var wChill = 35.74 + 0.6215 * weatherInfo.main.temp - 35.75 * Math.pow(weatherInfo.wind.speed, 0.16)
+        + 0.4275 * weatherInfo.main.temp * Math.pow (weatherInfo.wind.speed, 0.16);
+wChill = Math.round(wChill);
+document.getElementById('wChill').innerHTML = wChill
+}
+
+//forcast
+//var weatherForcast = new XMLHttpRequest();
