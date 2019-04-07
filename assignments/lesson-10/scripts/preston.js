88d9b6578926279a761a638613c9088d9a68988f
Kirk Mortenson <kmortenson@byui.edu>
2019-04-06 17:04:28 -0700

    fix

--- assignments/lesson-10/scripts/preston.js
+++ assignments/lesson-10/scripts/preston.js
@@ -5,7 +5,7 @@ var weatherObject = new XMLHttpRequest();
 weatherObject.open('GET', 'HTTPS://api.openweathermap.org/data/2.5/weather?id=5604473&appid=9a639e9b88f8f96c233986dc5ceb5877&units=imperial', true);
 weatherObject.send();
 weatherObject.onload = function () {
-    
+
     var weatherInfo = JSON.parse(weatherObject.responseText);
     console.log(weatherInfo);
 
@@ -14,11 +14,10 @@ weatherObject.onload = function () {
     document.getElementById('humidity').innerHTML = weatherInfo.main.humidity;
     document.getElementById('wSpeed').innerHTML = weatherInfo.wind.speed;
 
-   
-    var wChill = 35.74 + 0.6215 * weatherInfo.main.temp - 35.75 * Math.pow(weatherInfo.wind.speed, 0.16)
-        + 0.4275 * weatherInfo.main.temp * Math.pow (weatherInfo.wind.speed, 0.16);
-wChill = Math.round(wChill);
-document.getElementById('wChill').innerHTML = wChill
+
+    var wChill = 35.74 + 0.6215 * weatherInfo.main.temp - 35.75 * Math.pow(weatherInfo.wind.speed, 0.16) + 0.4275 * weatherInfo.main.temp * Math.pow(weatherInfo.wind.speed, 0.16);
+    wChill = Math.round(wChill);
+    document.getElementById('wChill').innerHTML = wChill;
 }
 
 //forcast
@@ -36,16 +35,16 @@ weatherForcast.onload = function () {
     for (i = 0; i < weatherInfo.list.length; ++i) {
         time = weatherInfo.list[i].dt_txt;
         if (time.includes("18:00:00")) {
-           
-            
-            var date = new Date(weatherInfo.list[i].dt * 1000);    
+
+
+            var date = new Date(weatherInfo.list[i].dt * 1000);
             var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
             var month = ["January", "February", "March", "April", "May", "June", "July", "August", "Saptember", "October", "November", "December"];
             var findDate = weekday[date.getDay()] + "<br>" + month[date.getMonth()] + " " + date.getDate();
             listDate.push(findDate);
 
             var temp = weatherInfo.list[i].main.temp_max;
-            var temp = Math.round(temp);
+            temp = Math.round(temp);
             listTemp.push(temp);
 
             var iconCode = weatherInfo.list[i].weather["0"].icon;
@@ -55,21 +54,23 @@ weatherForcast.onload = function () {
         continue;
     }
 
-//forcast date
-document.getElementById('day1').innerHTML = listDate[0];
-document.getElementById('day2').innerHTML = listDate[1];
-document.getElementById('day3').innerHTML = listDate[2];
-document.getElementById('day4').innerHTML = listDate[3];
-document.getElementById('day5').innerHTML = listDate[4];
-//Icon
-document.getElementById('weater_icon1').src = listIconCode[0];
-document.getElementById('weater_icon2').src = listIconCode[1];
-document.getElementById('weater_icon3').src = listIconCode[2];
-document.getElementById('weater_icon4').src = listIconCode[3];
-document.getElementById('weater_icon5').src = listIconCode[4];
-//Temp
-document.getElementById('highTemp1').innerHTML = listTemp[0];
-document.getElementById('highTemp2').innerHTML = listTemp[1];
-document.getElementById('highTemp3').innerHTML = listTemp[2];
-document.getElementById('highTemp4').innerHTML = listTemp[3];
-document.getElementById('highTemp5').innerHTML = listTemp[4];
+    //forcast date
+    document.getElementById('day1').innerHTML = listDate[0];
+    document.getElementById('day2').innerHTML = listDate[1];
+    document.getElementById('day3').innerHTML = listDate[2];
+    document.getElementById('day4').innerHTML = listDate[3];
+    document.getElementById('day5').innerHTML = listDate[4];
+    //Icon
+    document.getElementById('weater_icon1').src = listIconCode[0];
+    document.getElementById('weater_icon2').src = listIconCode[1];
+    document.getElementById('weater_icon3').src = listIconCode[2];
+    document.getElementById('weater_icon4').src = listIconCode[3];
+    document.getElementById('weater_icon5').src = listIconCode[4];
+    //Temp
+    document.getElementById('highTemp1').innerHTML = listTemp[0];
+    document.getElementById('highTemp2').innerHTML = listTemp[1];
+    document.getElementById('highTemp3').innerHTML = listTemp[2];
+    document.getElementById('highTemp4').innerHTML = listTemp[3];
+    document.getElementById('highTemp5').innerHTML = listTemp[4];
+    
+}
