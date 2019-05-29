f844d2ca140f15e72a46a36c04640b02743f67ce
Kirk Mortenson <kmortenson@byui.edu>
2019-05-28 23:42:36 -0700

    update

--- assignments/lesson-7/scripts/map.js
+++ assignments/lesson-7/scripts/map.js
@@ -1,12 +1,12 @@
 // Map for preston
 function myMap() {
-    var mapProp= {
-      center:new google.maps.LatLng(42.0963,-111.8766),
-      zoom:10,
-    };
-    var map = new google.maps.Map(document.getElementById("googlemap"),mapProp);
+  var mapProp= {
+    center:new google.maps.LatLng(42.0963,-111.8766),
+    zoom:10,
+  };
+  var map = new google.maps.Map(document.getElementById("googlemap"),mapProp);
 }
-    
+  
 // Map for Spda Springs
 function myMap2() {
     var mapProp= {
