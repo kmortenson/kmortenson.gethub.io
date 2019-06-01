e33fbf6543d248e28441a6ecea6a82eefbf3874c
Kirk Mortenson <kmortenson@byui.edu>
2019-05-31 20:56:59 -0700

    wind chill

--- assignments/lesson-6/scripts/windchill.js
+++ assignments/lesson-6/scripts/windchill.js
@@ -1,5 +1,5 @@
-var temp = parseInt(document.getElementById('cTemp').innerHTML);
-var speed = parseInt(document.getElementById('wSpeed').innerHTML);
+var temp = 80 //parseInt(document.getElementById('cTemp').innerHTML);
+var speed = 25//parseInt(document.getElementById('wSpeed').innerHTML);
 var wChill = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
 wChill = Math.round(wChill);
 document.getElementById("wChill").innerHTML = wChill
