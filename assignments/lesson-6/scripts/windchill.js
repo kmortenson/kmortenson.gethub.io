ad92efda104f7cb54d944e74fc042e388a85b93f
Kirk Mortenson <kmortenson@byui.edu>
2019-05-31 21:09:05 -0700

    change temp

--- assignments/lesson-6/scripts/windchill.js
+++ assignments/lesson-6/scripts/windchill.js
@@ -1,5 +1,3 @@
-//var cTemp = 80
-//var wSpeed = 25
 var temp = parseInt(document.getElementById('cTemp').innerHTML);
 var speed = parseInt(document.getElementById('wSpeed').innerHTML);
 var wChill = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed, 0.16) + 0.4275 * temp * Math.pow(speed, 0.16);
