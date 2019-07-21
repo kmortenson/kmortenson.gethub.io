3388a48fc1dd8c0ebcdedae083c0898a380b71c0
Kirk Mortenson <kmortenson@byui.edu>
2019-07-20 22:41:52 -0700

    JS files

--- assignments/weathersite/scripts/idahomap.js
+++ assignments/weathersite/scripts/idahomap.js
@@ -1,5 +1,43 @@
-// Map for Idaho
+// Map for Default
 function myMap() {
+  var mapProp = {
+    center: new google.maps.LatLng(44.714389, -114.699917),
+    zoom: 5,
+  };
+  var map = new google.maps.Map(document.getElementById("googlemap"), mapProp);
+  
+  
+  
+  // Map for D.C. Temple
+function myMap1() {
+  var mapProp = {
+    center: new google.maps.LatLng(44.714389, -114.699917),
+    zoom: 5,
+  };
+  var map = new google.maps.Map(document.getElementById("googlemap"), mapProp);
+}
+
+// Map for Salt Lake Temple
+function myMap2() {
+  var mapProp = {
+    center: new google.maps.LatLng(44.714389, -114.699917),
+    zoom: 5,
+  };
+  var map = new google.maps.Map(document.getElementById("googlemap"), mapProp);
+}
+
+
+// Map for Rexburg Temple
+function myMap3() {
+  var mapProp = {
+    center: new google.maps.LatLng(44.714389, -114.699917),
+    zoom: 5,
+  };
+  var map = new google.maps.Map(document.getElementById("googlemap"), mapProp);
+}
+
+// Map for St.George Temple
+function myMap4() {
   var mapProp = {
     center: new google.maps.LatLng(44.714389, -114.699917),
     zoom: 5,
