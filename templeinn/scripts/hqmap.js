7b907a3084ede3f222dd18eded8f9d4ef757f072
Kirk Mortenson <kmortenson@byui.edu>
2019-07-26 12:54:05 -0700

    update

---
+++ templeinn/scripts/hqmap.js
@@ -0,0 +1,16 @@
+// Map for Headquaters
+function myMap() {
+  var mapProp = {
+    center: new google.maps.LatLng(38.9830094, -77.0955393),
+    zoom: 15,
+  };
+  var map = new google.maps.Map(document.getElementById("googlemap"), mapProp);
+}
+
+
+/*
+DC 39.0100766, -77.0520085
+salt lake 40.7715502,-111.9007878
+Rexburg 43.8107968,-111.7773022
+St George 37.093983,-113.5868377
+*/
