cf4297d6b75eeced65efe1a9b643fe290b0e8d83
Kirk Mortenson <46416608+kmortenson@users.noreply.github.com>
2020-04-06 22:03:31 -0700

    final

--- TicTacToe/script.js
+++ TicTacToe/script.js
@@ -1,119 +1,133 @@
 var origBoard;
-const huPlayer = 'O';
-const aiPlayer = 'X';
+const huPlayer = "O";
+const aiPlayer = "X";
 const winCombos = [
-	[0, 1, 2],
-	[3, 4, 5],
-	[6, 7, 8],
-	[0, 3, 6],
-	[1, 4, 7],
-	[2, 5, 8],
-	[0, 4, 8],
-	[6, 4, 2]
-]
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [6, 4, 2]
+];
 
-const cells = document.querySelectorAll('.cell');
+const cells = document.querySelectorAll(".cell");
 startGame();
-// initate game and clear board
+
 function startGame() {
-	Quote();
-	document.querySelector(".endgame").style.display = "none";
-	origBoard = Array.from(Array(9).keys());
-	for (var i = 0; i < cells.length; i++) {
-		cells[i].innerText = '';
-		cells[i].style.removeProperty('background-color');
-		cells[i].addEventListener('click', turnClick, false);
-	}
+  clearWin();
+  Quote();
+  document.querySelector(".endgame").style.display = "none";
+  origBoard = Array.from(Array(9).keys());
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].innerText = "";
+    cells[i].style.removeProperty("background-color");
+    cells[i].addEventListener("click", turnClick, false);
+  }
 }
 
 function turnClick(square) {
-	if (typeof origBoard[square.target.id] == 'number') {
-		turn(square.target.id, huPlayer)
-		if (!checkTie()) turn(bestSpot(), aiPlayer);
-	}
+  if (typeof origBoard[square.target.id] == "number") {
+    turn(square.target.id, huPlayer);
+    if (!checkTie()) turn(bestSpot(), aiPlayer);
+  }
 }
-//take turn and verify if won
+
 function turn(squareId, player) {
-	origBoard[squareId] = player;
-	document.getElementById(squareId).innerText = player;
-	let gameWon = checkWin(origBoard, player)
-	if (gameWon) gameOver(gameWon)
+  origBoard[squareId] = player;
+  document.getElementById(squareId).innerText = player;
+  let gameWon = checkWin(origBoard, player);
+  if (gameWon) gameOver(gameWon);
 }
 
 function checkWin(board, player) {
-	let plays = board.reduce((a, e, i) =>
-		(e === player) ? a.concat(i) : a, []);
-	let gameWon = null;
-	for (let [index, win] of winCombos.entries()) {
-		if (win.every(elem => plays.indexOf(elem) > -1)) {
-			gameWon = {
-				index: index,
-				player: player
-			};
-			break;
-		}
-	}
-	return gameWon;
+  let plays = board.reduce((a, e, i) => (e === player ? a.concat(i) : a), []);
+  let gameWon = null;
+  for (let [index, win] of winCombos.entries()) {
+    if (win.every((elem) => plays.indexOf(elem) > -1)) {
+      gameWon = { index: index, player: player };
+      break;
+    }
+  }
+  return gameWon;
 }
 
 function gameOver(gameWon) {
-	for (let index of winCombos[gameWon.index]) {
-		document.getElementById(index).style.backgroundColor =
-			gameWon.player == huPlayer ? "blue" : "red";
-	}
-	for (var i = 0; i < cells.length; i++) {
-		cells[i].removeEventListener('click', turnClick, false);
-	}
-	declareWinner(gameWon.player == huPlayer ? "You win!" : "You lose.");
+  for (let index of winCombos[gameWon.index]) {
+    document.getElementById(index).style.backgroundColor =
+      gameWon.player == huPlayer ? "blue" : "red";
+  }
+  for (var i = 0; i < cells.length; i++) {
+    cells[i].removeEventListener("click", turnClick, false);
+  }
+  declareWinner(gameWon.player == huPlayer ? "You win!" : "You lose.");
 }
 
 function declareWinner(who) {
+  var win = document.createElement("DIV");
+  var text = document.createElement("DIV");
+  win.classList.add("end-animate");
+  text.classList.add("text");
+  win.appendChild(text);
 
-	document.querySelector(".text").innerText = who;
-	document.querySelector(".endgame").classList.add = "endgame-animate";
-	document.querySelector(".endgame").classList.remove = "endgame";
-	
+  document.body.appendChild(win);
+  document.querySelector(".endgame").style.display = "block";
+  document.querySelector(".text").innerText = who;
+  // document.getElementById("end").style.transform = "rotate(300deg) scale(2)";
 }
 
 function emptySquares() {
-	return origBoard.filter(s => typeof s == 'number');
+  return origBoard.filter((s) => typeof s == "number");
 }
 
 function bestSpot() {
-	return emptySquares()[0];
+  return emptySquares()[0];
 }
 
 function checkTie() {
-	if (emptySquares().length == 0) {
-		for (var i = 0; i < cells.length; i++) {
-			cells[i].style.backgroundColor = "green";
-			cells[i].removeEventListener('click', turnClick, false);
-		}
-		declareWinner("Tie Game!")
-		return true;
-	}
-	return false;
+  if (emptySquares().length == 0) {
+    for (var i = 0; i < cells.length; i++) {
+      cells[i].style.backgroundColor = "green";
+      cells[i].removeEventListener("click", turnClick, false);
+    }
+    declareWinner("Tie Game!");
+    return true;
+  }
+  return false;
 }
 
-// quote generator 
+// quote generator
 function Quote() {
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", "https://type.fit/api/quotes", true);
-	xhr.send();
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "https://type.fit/api/quotes", true);
+  xhr.send();
 
-	xhr.addEventListener("readystatechange", processRequest, false);
+  xhr.addEventListener("readystatechange", processRequest, false);
 
-	function processRequest() {
-		if (xhr.readyState == 4 && xhr.status == 200) {
-			var notify = JSON.parse(xhr.responseText);
-			var max = notify.length;
-			var i = randomQuote(max);
-			document.getElementById("quote").innerHTML = notify[i].text + "<br/>Author: " + notify[i].author;
-
-		}
-	}
+  function processRequest() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      var notify = JSON.parse(xhr.responseText);
+      var max = notify.length;
+      var i = randomQuote(max);
+      document.getElementById("quote").innerHTML =
+        notify[i].text + "<br/>Author: " + notify[i].author;
+    }
+  }
 }
 //random quote generator
 function randomQuote(max) {
-	return Math.floor(Math.random() * (max - 0)) + 0;
+  return Math.floor(Math.random() * (max - 0)) + 0;
+}
+
+function clearWin() {
+  var text = document.getElementsByClassName("text");
+  while (text[0]) {
+    text[0].parentNode.removeChild(text[0]);
+  }
+
+  var win = document.getElementsByClassName("end-animate");
+  while (win[0]) {
+    win[0].parentNode.removeChild(win[0]);
+  }
 }
